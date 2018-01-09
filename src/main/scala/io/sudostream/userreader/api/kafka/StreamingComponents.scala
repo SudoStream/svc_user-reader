bc0886bdf696f9e6873fa0e3d08a53925494c151
Andy Boyle <andy@sudostream.io>
2018-01-09 15:57:20 +0000

    0.0.1-31 : Refactor the SASL config to be producer independent

--- src/main/scala/io/sudostream/userreader/api/kafka/StreamingComponents.scala
+++ src/main/scala/io/sudostream/userreader/api/kafka/StreamingComponents.scala
@@ -1,9 +1,11 @@
 package io.sudostream.userreader.api.kafka
 
 import akka.actor.ActorSystem
+import akka.event.LoggingAdapter
 import akka.kafka.ProducerSettings
 import akka.stream.Materializer
 import io.sudostream.timetoteach.kafka.serializing.SystemEventSerializer
+import io.sudostream.timetoteach.messages.events.SystemEvent
 import io.sudostream.userreader.config.{ActorSystemWrapper, ConfigHelper}
 import org.apache.kafka.clients.CommonClientConfigs
 import org.apache.kafka.common.config.SaslConfigs
@@ -15,19 +17,20 @@ class StreamingComponents(configHelper: ConfigHelper, actorSystemWrapper: ActorS
   implicit val system: ActorSystem = actorSystemWrapper.system
   implicit val executor: ExecutionContextExecutor = system.dispatcher
   implicit val materializer: Materializer = actorSystemWrapper.materializer
-  val log = system.log
+  val log: LoggingAdapter = system.log
 
-  lazy val kafkaProducerBootServers = configHelper.config.getString("akka.kafka.producer.bootstrapservers")
-  lazy val kafkaProducerSaslJaasUsername = configHelper.config.getString("akka.kafka.producer.saslJassUsername")
-  lazy val kafkaProducerSaslJaasPassword = configHelper.config.getString("akka.kafka.producer.saslJassPassword")
-  lazy val kafkaProducerSaslJaasConfig = s"org.apache.kafka.common.security.scram.ScramLoginModule required " +
-    s"""username="$kafkaProducerSaslJaasUsername" password="$kafkaProducerSaslJaasPassword";"""
+  lazy val kafkaProducerBootServers: String = configHelper.config.getString("akka.kafka.producer.bootstrapservers")
+  lazy val kafkaSaslJaasUsername: String = configHelper.config.getString("akka.kafka.saslJassUsername")
+  lazy val kafkaSaslJaasPassword: String = configHelper.config.getString("akka.kafka.saslJassPassword")
+  lazy val kafkaSaslJaasConfig: String = s"org.apache.kafka.common.security.scram.ScramLoginModule required " +
+    s"""username="$kafkaSaslJaasUsername" password="$kafkaSaslJaasPassword";"""
 
-  val producerSettings = ProducerSettings(system, new ByteArraySerializer, new SystemEventSerializer)
-    .withBootstrapServers(kafkaProducerBootServers)
-    .withProperty(SaslConfigs.SASL_JAAS_CONFIG, kafkaProducerSaslJaasConfig)
-    .withProperty(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-256")
-    .withProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL")
+  val producerSettings: ProducerSettings[Array[Byte], SystemEvent] =
+    ProducerSettings(system, new ByteArraySerializer, new SystemEventSerializer)
+      .withBootstrapServers(kafkaProducerBootServers)
+      .withProperty(SaslConfigs.SASL_JAAS_CONFIG, kafkaSaslJaasConfig)
+      .withProperty(SaslConfigs.SASL_MECHANISM, "SCRAM-SHA-256")
+      .withProperty(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL")
 
   log.info(s"Systems event topic is '$definedSystemEventsTopic'")
 
