479cbc9a79beeb9c157fa828db1445adc025546e
Andy Boyle <andy@sudostream.io>
2018-01-09 15:06:30 +0000

    0.0.1-27 : Remove debug statement

--- src/main/scala/io/sudostream/userreader/api/kafka/StreamingComponents.scala
+++ src/main/scala/io/sudostream/userreader/api/kafka/StreamingComponents.scala
@@ -23,8 +23,6 @@ class StreamingComponents(configHelper: ConfigHelper, actorSystemWrapper: ActorS
   lazy val kafkaProducerSaslJaasConfig = s"org.apache.kafka.common.security.scram.ScramLoginModule required " +
     s"""username="$kafkaProducerSaslJaasUsername" password="$kafkaProducerSaslJaasPassword";"""
 
-  log.debug(s"kafkaProducerSaslJaasConfig = '$kafkaProducerSaslJaasConfig'")
-
   val producerSettings = ProducerSettings(system, new ByteArraySerializer, new SystemEventSerializer)
     .withBootstrapServers(kafkaProducerBootServers)
     .withProperty(SaslConfigs.SASL_JAAS_CONFIG, kafkaProducerSaslJaasConfig)
